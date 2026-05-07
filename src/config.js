d3d67106ace3ad6b60e1238ebbcfff231aca6749
Love Pareek <monupareeklg@gmail.com>
2026-05-07 13:36:29 +0530

    feat: add npmignore file and remove old code

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  middlePrice: 305.23,
-  oneDayBeforePrice: 304.21,
-  simulationInterval: 1000, // Trade simulation interval
-  updateInterval: 2500, // Update payload interval,
-  defaultTimezoneOffset: 5.5, // IST (UTC+5:30)
-  supportedMarkets: ["crypto", "forex", "equity"],
-};
